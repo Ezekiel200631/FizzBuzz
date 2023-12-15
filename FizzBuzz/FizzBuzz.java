31b9aee3e4d94d38e777533e9265e5831ba39a67
Ezekiel Lopez <ezekiel.lopez@mrjaffesclass.com>
2023-12-15 10:52:52 -0800

    Commit message

--- FizzBuzz/FizzBuzz.java
+++ FizzBuzz/FizzBuzz.java
@@ -1,20 +1,38 @@
 public class FizzBuzz
+{ 
+    public static void main(String[] args) 
+    { 
+        for (int i = 1; i <= 100; i++) 
+        { 
+            boolean isFizz = false; 
+            boolean isBuzz = false; 
 
-{ public static void main(String[] args) 
+            if (i / 3 * 3 == i) 
+            { 
+                isFizz = true; 
+            } 
 
-    { for (int i = 1; i <= 100; i++) 
-        { boolean isFizz = false; boolean isBuzz = false; if (i / 3 * 3 == i) 
-            { isFizz = true; } if (i / 5 * 5 == i) 
+            if (i / 5 * 5 == i) 
+            { 
+                isBuzz = true; 
+            } 
 
-            { isBuzz = true; } if (isFizz && isBuzz) 
-
-            { System.out.println("FizzBuzz"); } else if (isFizz) 
-
-            { System.out.println("Fizz"); } else if (isBuzz) 
-
-            { System.out.println("Buzz"); } else 
-
-            { System.out.println(i); } 
+            if (isFizz && isBuzz) 
+            { 
+                System.out.println("FizzBuzz"); 
+            } 
+            else if (isFizz) 
+            { 
+                System.out.println("Fizz"); 
+            } 
+            else if (isBuzz) 
+            { 
+                System.out.println("Buzz"); 
+            } 
+            else 
+            { 
+                System.out.println(i); 
+            } 
         } 
     }
 }
